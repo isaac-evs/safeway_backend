be69eb946585743a110ab783d3591c0ebee2f54c
carlosssfelixxx <carlosfs16@hotmail.com>
2025-05-09 13:20:04 -0700

    CORS configuration

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from routes import auth, news
 from database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(news.router)
