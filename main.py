f5975f470fea9597b860bb29c1e985dbd5777760
Isaac <isaac.vazquez@iteso.mx>
2025-05-16 04:07:46 -0600

    added health ednpoint

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from routes import auth, news
 from database import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
 
-
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -15,6 +14,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+def read_root():
+    return {"status": "healthy", "message": "Safeway API is running"}
 
 app.include_router(auth.router)
 app.include_router(news.router)
